fd2433e880662f6c02a8b602b4f5aacbe35221cd
Kohe Tokoi <tokoik@gmail.com>
2012-03-01 01:32:58 +0900

    Initial Commit

---
+++ extrusion/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  extrusion
+//
+//  Created by 床井 浩平 on 12/03/01.
+//  Copyright (c) 2012年 和歌山大学. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+  // insert code here...
+  std::cout << "Hello, World!\n";
+    return 0;
+}
+
