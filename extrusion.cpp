197e0438df9488c8c3e19107da01eb11e32ba909
Kohe Tokoi <tokoi@wakayama-u.ac.jp>
2026-05-01 09:23:47 +0900

    refactor: optimize extrusion mesh generation logic for improved performance

--- extrusion.cpp
+++ extrusion.cpp
@@ -1,13 +1,22 @@
-﻿#include <math.h>
-#if defined(WIN32)
-#  pragma comment(linker, "/subsystem:\"windows\" /entry:\"mainCRTStartup\"")
-#  undef GL_GLEXT_PROTOTYPES
-#  include "glut.h"
-#elif defined(__APPLE__) || defined(MACOSX)
+﻿#if defined(__APPLE__) || defined(MACOSX)
+#  define GL_SILENCE_DEPRECATION
 #  include <GLUT/glut.h>
 #else
+#  if defined(_WIN32)
+#    define _USE_MATH_DEFINES
+#    define _CRT_SECURE_NO_WARNINGS
+#  endif
 #  include <GL/glut.h>
+#  if defined(_WIN32)
+#    if !defined(GL_CLAMP_TO_EDGE)
+#      define GL_CLAMP_TO_EDGE 0x812F
+#    endif
+//#    pragma comment(linker, "/subsystem:\"windows\" /entry:\"mainCRTStartup\"")
+#  endif
 #endif
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
 #include "extrusion.h"
 
